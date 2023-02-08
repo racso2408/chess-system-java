b5703aa0c7c80bf433dd2b18288cba043ccbad87
Oscar Cavalcanti <oscarmcavalcanti@gmail.com>
2023-02-08 11:32:23 -0300

    Special move - Promotion

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,19 +1,19 @@
 package application;
 
-import boardgame.Board;
-import boardgame.Position;
-import chess.ChessException;
-import chess.ChessMatch;
-import chess.ChessPiece;
-import chess.ChessPosition;
-
 import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
 
+import chess.ChessException;
+import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
+
 public class Program {
+
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
         ChessMatch chessMatch = new ChessMatch();
         List<ChessPiece> captured = new ArrayList<>();
@@ -29,25 +29,31 @@ public class Program {
                 boolean[][] possibleMoves = chessMatch.possibleMoves(source);
                 UI.clearScreen();
                 UI.printBoard(chessMatch.getPieces(), possibleMoves);
-
                 System.out.println();
                 System.out.print("Target: ");
                 ChessPosition target = UI.readChessPosition(sc);
 
                 ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
 
-                if (capturedPiece != null){
+                if (capturedPiece != null) {
                     captured.add(capturedPiece);
                 }
-            } catch (ChessException e) {
+
+                if (chessMatch.getPromoted() != null) {
+                    System.out.print("Enter piece for promotion (B/N/R/Q): ");
+                    String type = sc.nextLine();
+                    chessMatch.replacePromotedPiece(type);
+                }
+            }
+            catch (ChessException e) {
                 System.out.println(e.getMessage());
                 sc.nextLine();
-            } catch (InputMismatchException e){
+            }
+            catch (InputMismatchException e) {
                 System.out.println(e.getMessage());
                 sc.nextLine();
             }
         }
-
         UI.clearScreen();
         UI.printMatch(chessMatch, captured);
     }
